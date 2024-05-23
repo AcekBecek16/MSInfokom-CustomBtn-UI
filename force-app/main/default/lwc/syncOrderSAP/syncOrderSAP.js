b635d1f807ceb3be7c90da00f8f67fa97f376391
AcekBecek <nurazispakaya16@gmail.com>
2024-05-23 20:14:35 +0700

    create LWC file for Btn Sync orderSAP

---
+++ force-app/main/default/lwc/syncOrderSAP/syncOrderSAP.js
@@ -0,0 +1,9 @@
+import { LightningElement, api, wire } from 'lwc';
+import { gql, graphql,refreshGraphQL } from "lightning/uiGraphQLApi";
+import { CloseActionScreenEvent } from 'lightning/actions';
+import { NavigationMixin } from "lightning/navigation";
+
+
+export default class SyncOrderSAP extends NavigationMixin(LightningElement) {
+    @api recordId
+}
