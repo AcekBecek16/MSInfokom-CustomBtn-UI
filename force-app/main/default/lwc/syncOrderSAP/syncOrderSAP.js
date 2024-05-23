17447ace99b9d95f0a580115000fa24f304cc947
AcekBecek <nurazispakaya16@gmail.com>
2024-05-23 20:59:18 +0700

    use description list???

--- force-app/main/default/lwc/syncOrderSAP/syncOrderSAP.js
+++ force-app/main/default/lwc/syncOrderSAP/syncOrderSAP.js
@@ -6,4 +6,21 @@ import { NavigationMixin } from "lightning/navigation";
 
 export default class SyncOrderSAP extends NavigationMixin(LightningElement) {
     @api recordId
+    headerLabel = 'Sync Order'
+
+
+    handleCancel(event){
+        this.dispatchEvent(new CloseActionScreenEvent())
+    }
+
+    navigateToRecord(recordId) {
+        this[NavigationMixin.Navigate]({
+          type: "standard__recordPage",
+          attributes: {
+            recordId,
+            actionName: "view",
+          },
+        });
+    }
+
 }
